e20781a17dcce008d3419cb0cf2c9ae9a99e949e
Anton Yevchenko <anton.evchenko@onicore.io>
2025-05-26 20:20:31 +0300

    [CGW] update minor logic

--- dist/routes/rain.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const common_routes_1 = require("./common-routes");
-const router = (0, express_1.Router)();
-(0, common_routes_1.attachCommonRoutes)(router, '/rain');
-exports.default = router;
